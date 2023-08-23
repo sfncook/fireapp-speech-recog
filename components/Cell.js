94b74840cc7b6f7583ef2b129f78d5c4102c45f3
Shawn Cook <sfncook@gmail.com>
2023-08-23 09:00:02 -0700

    layout grid

---
+++ components/Cell.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { ReactSketchCanvas } from "react-sketch-canvas";
+import styles from '@/styles/Cell.module.css'
+
+export default function Cell({  }) {
+  const canvas = React.useRef()
+
+  return (
+    <div className={styles.griditem}>
+      fuck
+    </div>
+  );
+}
