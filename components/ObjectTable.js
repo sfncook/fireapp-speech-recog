9154050db54b386dcdbbc9bb0bbb5fc2164a5a77
Shawn Cook <sfncook@gmail.com>
2023-09-01 11:16:16 -0700

    Table style

--- components/ObjectTable.js
+++ components/ObjectTable.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import styles from '@/styles/ObjectTable.module.css'
 
 const ObjectTable = ({ data }) => {
+
+    function formatCamelCaseString(input) {
+        const spacedString = input.replace(/([A-Z])/g, ' $1'); // Add spaces before capital letters
+        return spacedString.toLowerCase().charAt(0).toUpperCase() + spacedString.slice(1); // Capitalize the first character
+    }
     const formatValue = (value) => {
         if (typeof value === 'boolean') {
             return value ? 'Yes' : 'No';
@@ -16,14 +22,14 @@ const ObjectTable = ({ data }) => {
     const renderTableRows = () => {
         return Object.keys(data).map((key, index) => (
             <tr key={index}>
-                <td>{key}</td>
-                <td>{formatValue(data[key])}</td>
+                <td className={styles.keyCell}>{formatCamelCaseString(key)}</td>
+                <td className={styles.valueCell}>{formatValue(data[key])}</td>
             </tr>
         ))
     };
 
     return (
-        <table>
+        <table className={styles.objectTable}>
             <thead>
             <tr>
                 <th>Key</th>
