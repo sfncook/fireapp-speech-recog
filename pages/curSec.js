9e8b7c4c541749d75eefd15a37d9f35bcaca6556
Shawn Cook <sfncook@gmail.com>
2023-09-04 12:26:20 -0700

    Update to use data locally in client

--- pages/curSec.js
+++ pages/curSec.js
@@ -3,90 +3,28 @@ import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/CurSec.module.css'
 import ObjectTable from "@/components/ObjectTable";
+import dataF2022119715 from '../data/F2022119715.json'
 
 const inter = Inter({ subsets: ['latin'] })
 const initState = {'radioChannel': '', 'incidentLocation': '', 'units': [], 'sectors': [], 'unitsToRoles': {}, 'nearestHydrant': '', 'symptomsOnScene': '', 'structureDescription': '', 'isWorkingFire': '', 'strategy': '', 'incidentClassification': '', '': 'unknown', 'victims': [], 'manyVictims': 0, 'waterNeeded': [], 'sectorsCleared': [], 'commandUnit': '', 'resourcesNeeded': [], 'isParRequested': false, 'parDeclared': []}
 
 export default function Home()  {
-  const [allSeconds, setAllSeconds] = useState([]);
-  const [processedSecs, setProcessedSecs] = useState([]);
   const [state, setState] = useState(initState);
   const [lastSpeech, setLastSpeech] = useState()
   const [isRecording, setIsRecording] = useState(false)
   const aud = useRef()
 
-  useEffect( () => {
-    try {
-      fetch("/api/curSec?getAllSec=1", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-          .then(async response=>{
-            const _allS = (await response.json()).map(i=>parseInt(i))
-            console.log(_allS)
-            setAllSeconds(_allS)
-          })
-    } catch(error) {
-      console.error(error);
-      // alert(error.message)
-    }
-  }, []);
-
-  const processSec = async curSec => {
-    if(allSeconds.includes(curSec) && !processedSecs.includes(curSec)) {
-      console.log(`process curSec:${curSec}`)
-      setIsRecording(true)
-      setProcessedSecs([...processedSecs, curSec])
-      try {
-        const response = await fetch("/api/curSec", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({curSec}),
-        });
-
-        const data = await response.json()
-        const _state = data.result.state
-        const speech = data.result.speech
-        const waitMs = (data.result.endSec - data.result.startSec)*1000
-        setTimeout(()=>{
-          setState(_state)
-          setLastSpeech(speech)
-          setIsRecording(false)
-        }, waitMs)
-      } catch(error) {
-        console.error(error);
-        alert(error.message)
-      }
-    }
-  }
-
   const onTimeUpdate = async () => {
-    const curSec = Math.floor(aud.current.currentTime)
-    processSec(curSec)
-  }
-
-  const findNextLower = (sortedArray, searchValue) => {
-    for (let i = 0; i < sortedArray.length - 1; i++) {
-      const lower = sortedArray[i]
-      const higher = sortedArray[i + 1]
-      // console.log(`lower:${lower} higher:${higher}`)
-      if ( lower <= searchValue && higher > searchValue) {
-        return sortedArray[i];
-      }
+    setIsRecording(dataF2022119715.find(d =>
+        aud.current.currentTime>=d.startSec &&
+        aud.current.currentTime<=d.endSec)
+    )
+    const curSec = Math.ceil(aud.current.currentTime)
+    const data = dataF2022119715.find(d => d.endSec === curSec)
+    if(data) {
+      setState(data.state)
+      setLastSpeech(data.speech)
     }
-    if(searchValue<sortedArray[0]) return sortedArray[0]
-    else return sortedArray.splice(-1);
-  }
-
-  const onSeeked = async () => {
-    const curSec = Math.floor(aud.current.currentTime)
-    const sortedSecs = [...allSeconds].sort((a, b) => a - b)
-    const nextLower = findNextLower(sortedSecs, curSec)
-    // console.log(`onSeeked: ${curSec} nextLower: ${nextLower}`)
   }
 
   const dotStyle = isRecording ? `${styles.dot} ${styles.recordingdot} ${styles.blinking}` : styles.dot
@@ -101,7 +39,6 @@ export default function Home()  {
             <div>House Fire @ 1564 West Linder</div>
             <audio controls src='audio/F2022119715_orig_regenerated.wav'
                    onTimeUpdate={onTimeUpdate}
-                   onSeeked={onSeeked}
                    ref={aud}
                    className={styles.radioAudio}
             />
